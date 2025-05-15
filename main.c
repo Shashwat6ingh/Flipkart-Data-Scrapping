33772f75fca5afcec73b625afe328b55cb578dbe
Shashwat singh <132599620+Shashwat6ingh@users.noreply.github.com>
2025-05-15 13:13:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    printf("welocme to c programming\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
+    printf("This is my first program\n");
+}
